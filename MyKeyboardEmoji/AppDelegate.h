a37e92b788808ee1c141b09d8f58bec6340a3c00
LV_FIRST_SAN <MY_FISTNAME_SAN@example.com>
2017-10-13 13:35:01 +0800

    Initial Commit

---
+++ MyKeyboardEmoji/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyKeyboardEmoji
+//
+//  Created by 吕超 on 2017/10/13.
+//  Copyright © 2017年 MAXTV. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
